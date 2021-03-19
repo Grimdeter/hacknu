3ff361904e54e8855eadba25ad4f1ca494d80797
AbsoluteSuccess <yerkali.ziyatov@gmail.com>
2021-03-20 05:33:39 +0600

    Design

--- src/Components/Leaders/Leaders.js
+++ src/Components/Leaders/Leaders.js
@@ -1,6 +1,7 @@
 import React from "react";
 /* Core CSS required for Ionic components to work properly */
 import "@ionic/react/css/core.css";
+import './Leaders.css';
 
 /* Basic CSS for apps built with Ionic */
 import "@ionic/react/css/normalize.css";
@@ -10,8 +11,20 @@ import "@ionic/react/css/typography.css";
 /* Theme variables */
 import "../../theme/variables.css";
 class Leaders extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      leaders: [],
+      points: [],
+    };
+  }
   render() {
-    return <p>Список лидеров</p>
+    const { leaders, points } = this.state;
+    return (
+      <div className="leaders">
+        <h1>Список лидеров</h1>
+      </div>
+    );
   }
 }
 
