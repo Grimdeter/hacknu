3cb2caabda0bc89a023271c3ee4c91d69ab12850
AbsoluteSuccess <yerkali.ziyatov@gmail.com>
2021-03-20 01:43:42 +0600

    clear app.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,6 @@
-import { useEffect, useRef, useState } from "react";
-import aituBridge from "@btsd/aitu-bridge";
-import {
-  IonApp,
-  IonContent,
-  IonButton,
-  IonToast,
-  IonText,
-  IonModal,
-} from "@ionic/react";
-
-import "./App.css";
-
-/* Core CSS required for Ionic components to work properly */
-import "@ionic/react/css/core.css";
-
-/* Basic CSS for apps built with Ionic */
-import "@ionic/react/css/normalize.css";
-import "@ionic/react/css/structure.css";
-import "@ionic/react/css/typography.css";
-
-/* Optional CSS utils that can be commented out */
-import "@ionic/react/css/padding.css";
-import "@ionic/react/css/float-elements.css";
-import "@ionic/react/css/text-alignment.css";
-import "@ionic/react/css/text-transformation.css";
-import "@ionic/react/css/flex-utils.css";
-import "@ionic/react/css/display.css";
-
-/* Theme variables */
-import "./theme/variables.css";
-
 const App = () => {
   return (
-    <IonApp>
-      <IonContent>
-        <p>Hey</p>
-      </IonContent>
-    </IonApp>
+    <p>hey</p>
   );
 };
 
