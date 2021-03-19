d210867e433511ab1ccfa4aca99ef91e4b9176c9
AbsoluteSuccess <yerkali.ziyatov@gmail.com>
2021-03-20 01:40:16 +0600

    configuration

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import { useEffect, useRef, useState } from "react";
 import aituBridge from "@btsd/aitu-bridge";
 import {
   IonApp,
-  IonSlides,
-  IonSlide,
   IonContent,
   IonButton,
+  IonToast,
   IonText,
+  IonModal,
 } from "@ionic/react";
 
 import "./App.css";
@@ -30,121 +30,11 @@ import "@ionic/react/css/display.css";
 /* Theme variables */
 import "./theme/variables.css";
 
-interface ISlideContentProps {
-  title: string;
-  onClick: () => void;
-  description: string;
-  buttonTitle: string;
-  imgSrc: string;
-}
-
-const SlideContent: React.FC<ISlideContentProps> = ({
-  onClick,
-  title,
-  description,
-  buttonTitle,
-  imgSrc,
-}) => {
-  return (
-    <>
-      <img src={imgSrc} />
-      <div className="slide-block">
-        <IonText color="dark">
-          <h2>{title}</h2>
-        </IonText>
-        <IonText>
-          <sub>{description}</sub>
-        </IonText>
-      </div>
-      <div className="slide-button">
-        <IonButton expand="full" onClick={onClick}>
-          {buttonTitle}
-        </IonButton>
-      </div>
-    </>
-  );
-};
-
-const App: React.FC = () => {
-  // Optional parameters to pass to the swiper instance.
-  // See http://idangero.us/swiper/api/ for valid options.
-  const slideOpts = {
-    initialSlide: 0,
-    speed: 400,
-  };
-  const slider = useRef<HTMLIonSlidesElement>(null);
-
-  async function getMe() {
-    try {
-      const data = await aituBridge.getMe();
-      setName(data.name);
-    } catch (e) {
-      // handle error
-      console.log(e);
-    }
-  }
-
-  useEffect(() => {
-    if (aituBridge.isSupported()) {
-      getMe();
-    }
-  }, []);
-
-  const [name, setName] = useState("<username>");
-
-  const handleButtonClick = () => {
-    slider.current?.slideNext();
-  };
-
+const App = () => {
   return (
     <IonApp>
       <IonContent>
-        <IonSlides pager={true} options={slideOpts} ref={slider}>
-          <IonSlide>
-            <SlideContent
-              title={`Привет, ${name}, Мини-приложения в Aitu`}
-              onClick={handleButtonClick}
-              description={
-                "Расскажем, что это и как использовать aitu.apps для своего бизнеса"
-              }
-              buttonTitle={"Я готов!"}
-              imgSrc={"/assets/slide1.png"}
-            ></SlideContent>
-          </IonSlide>
-          <IonSlide>
-            <SlideContent
-                title={"+800.000 пользователей Aitu"}
-                onClick={handleButtonClick}
-                description={
-                  "Могут увидеть ваше мини-приложение и стать его пользователями"
-                }
-                buttonTitle={"Интересно"}
-                imgSrc={"/assets/slide2.png"}
-            ></SlideContent>
-          </IonSlide>
-          <IonSlide>
-            <SlideContent
-                title={"Всегда под рукой "}
-                onClick={handleButtonClick}
-                description={
-                  "Каталог с мини-приложениями находится на центральной вкладке. Пользователи легко его найдут"
-                }
-                buttonTitle={"Что ещё?"}
-                imgSrc={"/assets/slide3.png"}
-            ></SlideContent>
-          </IonSlide>
-          <IonSlide>
-            <SlideContent
-                title={"Баннер с ваши предложением"}
-                onClick={handleButtonClick}
-                description={
-                  "         Уникальная скидка, спецпредложение или акция. Донесите ценное предложение до всех пользователей Aitu"
-                }
-                buttonTitle={"Далее"}
-                imgSrc={"/assets/slide4.png"}
-            ></SlideContent>
-          </IonSlide>
-        </IonSlides>
+        <p>Hey</p>
       </IonContent>
     </IonApp>
   );
